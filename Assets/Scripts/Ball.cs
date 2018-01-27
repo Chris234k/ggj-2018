c25f47723ea42335b29ea97a113dc436d93405b1
Chris Bruno <Chris234k@gmail.com>
2018-01-27 12:10:40 -0500

    Ball + recall

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Ball : MonoBehaviour 
+{
+	Rigidbody2D localRigid;
+	private bool isRecalling;
+
+	void Awake()
+	{
+		localRigid = GetComponent<Rigidbody2D>();
+	}
+
+	public void Attack() 
+	{
+
+	}
+
+	public void Throw(Vector2 force)
+	{
+		localRigid.AddForce(force);
+	}
+
+	public void Recall(Vector2 pos)
+	{
+		if(!isRecalling)
+		{
+			StartCoroutine(RecallRoutine(pos, 1f));
+		}
+	}
+
+	IEnumerator RecallRoutine(Vector2 pos, float duration)
+	{
+		isRecalling = true;
+		localRigid.isKinematic = true;
+		localRigid.velocity = Vector2.zero;
+		
+		float elapsedTime = 0;
+		while(elapsedTime < duration)
+		{
+			float t = elapsedTime / duration;
+			transform.position = Vector2.Lerp(transform.position, pos, t);
+
+			yield return new WaitForEndOfFrame();
+			elapsedTime += Time.deltaTime;
+		}
+		localRigid.isKinematic = false;
+		isRecalling = false;
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour 
 {
+    public Ball ball;
+
     public float acceleration;
     public float maxSpeed;
     public float friction;
+
+    public float throwForce;
     Vector3 vel;
+
+    void Start()
+    {
+        ball.Recall(transform.position);
+    }
 
     void Update()
     {
@@ -23,5 +32,17 @@ public class Player : MonoBehaviour
         vel *= friction;
 
         transform.position = transform.position + vel;
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 world_mouse_pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 dir = world_mouse_pos - transform.position;
+            Vector3 force = dir.normalized * throwForce;
+            ball.Throw(force);
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            ball.Recall(transform.position);
+        }
     }
 }
