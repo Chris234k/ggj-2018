657dfecfbb9d4578c5bb8319121fc68994560911
MARRRRRRRRRRRRRRRRRRRTTTY <martin.wong.512@gmail.com>
2018-01-27 19:55:54 -0500

    Adding follow camera. Adding player and ball prefab.

---
+++ Assets/_GGJ2018/Scripts/CameraFollow.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    private float interpVelocity;
+    public float CameraSnapSpeed = 5.0f;
+    public GameObject target;
+    public Vector3 offset;
+    Vector3 targetPos;
+    // Use this for initialization
+    void Start()
+    {
+        targetPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (target)
+        {
+            Vector3 posNoZ = transform.position;
+            posNoZ.z = target.transform.position.z;
+
+            Vector3 targetDirection = (target.transform.position - posNoZ);
+
+            interpVelocity = targetDirection.magnitude * CameraSnapSpeed;
+
+            targetPos = transform.position + (targetDirection.normalized * interpVelocity * Time.deltaTime);
+            transform.position = Vector3.Lerp(transform.position, targetPos + offset, 0.25f);
+
+        }
+    }
+}
